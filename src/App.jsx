ccef6818ddfa05d129f0a194175a956b4790ad18
Home <h63966239@gmail.com>
2026-04-06 19:54:48 +0700

    Fix import path for Win98Desktop component

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 import Win98Desktop from '../components/Win98Desktop'
 =======
 import Win98Desktop from './components/Win98Desktop'
->>>>>>> c82351598965e9d500e53a0596429fbad2f1a0fe
+>>>>>>>  
 import './App.css'
 
 export default function App() {
